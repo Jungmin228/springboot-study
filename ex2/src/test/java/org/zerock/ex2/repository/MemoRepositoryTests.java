848d7b1b4a7ef399a096d7e084911d8ca3463e07
Moomin <moominK228@gmail.com>
2022-07-10 22:20:56 +0900

    Feat: 네이밍 쿼리문 작성
    - TC 도 함께 작성하였음

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -112,9 +114,32 @@ public class MemoRepositoryTests {
 
         Page<Memo> result = memoRepository.findAll(pageable);
 
-        result.get().forEach(memo -> {
+        result.get().forEach(System.out::println);
+    }
+
+    @Test
+    void testQueryMethods() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+        for (Memo memo : list) {
             System.out.println(memo);
-        });
+        }
+    }
+
+    @Test
+    void testQueryMethodWithPageable() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    void testDeleteQueryMethods() {
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 
 }
